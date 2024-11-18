5d477022a33f054f0b828cc3ed55ccc372a8adf6
Dharanivel <90686595+Dharanive@users.noreply.github.com>
2024-11-18 17:39:57 +0530

    Update client.ts

--- src/app/client.ts
+++ src/app/client.ts
@@ -2,7 +2,7 @@ import { createThirdwebClient } from "thirdweb";
 
 // Replace this with your client ID string
 // refer to https://portal.thirdweb.com/typescript/v5/client on how to get a client ID
-const clientId =ee71f8b3290c646a0e945a984b6c432c;
+const clientId ="ee71f8b3290c646a0e945a984b6c432c";
 
 if (!clientId) {
   throw new Error("No client ID provided");
